db8b659e94143ab317c2fd4dd94aa4b943d59c81
Devuatl <kieda.lukasz@gmail.com>
2012-08-12 07:03:09 +0200

    Initial RoR app with execjs and therubyracer.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySecondApp::Application.config.secret_token = 'e36dbdc21966be4372f930b768b05862bd85a511265fd09a8282d75610b408b50de4600e6bb05b2a815a67e3dee3c6e0a426d48efe0426c19516075c33e9ce51'
